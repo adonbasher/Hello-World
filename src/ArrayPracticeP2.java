1d09f4851ce347b12f68a4481fd3531794d9efdd
adamiano <adamiano@student.mamkschools.org>
2018-09-20 09:57:54 -0400

    Initial Commit

--- src/ArrayPracticeP2.java
+++ src/ArrayPracticeP2.java
@@ -1,6 +1,7 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
+
 public class ArrayPracticeP2 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
@@ -10,32 +11,34 @@ public class ArrayPracticeP2 {
     public static void userInput(Scanner console) {
         System.out.print("How many days' temperatures? ");
         int days = console.nextInt();
-        int[] temps = new int[days+1];
+        int[] temps = new int[days + 1];
         dayTemps(days, console, temps);
     }
 
     public static void dayTemps(int days, Scanner console, int[] temps) {
         int highSum = 0;
         int dayHigh;
-        for(int x=1; x<=days; x++) {
+        for (int x = 1; x <= days; x++) {
             System.out.print("Day " + x + "'s high temp: ");
             dayHigh = console.nextInt();
             temps[x] = dayHigh;
             highSum += dayHigh;
         }
-        double averageTemp = (1.0 * highSum)/(1.0 *days);
+        double averageTemp = (Math.round((1.0 * highSum) / (1.0 * days)) * 100.0) / 100.0;
         System.out.println("Average temp = " + averageTemp);
-int daysAbove = 0;
-        for (int x=1; x<=days; x++) {
+        int daysAbove = 0;
+        for (int x = 1; x <= days; x++) {
             if (temps[x] > averageTemp) {
                 daysAbove++;
             }
         }
         System.out.println(daysAbove + " days were above average.");
-        System.out.println("Tempuratures: ");
+        /*
+        System.out.print("Tempuratures: ");
 
         System.out.println(Arrays.toString(temps));
         System.out.println("Two coldest days");
 
+        */
     }
 }
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main (String[] cheese) {
-
         System.out.println("Hello World!");
+        System.out.println("Hello World!2");
+
     }
 }
