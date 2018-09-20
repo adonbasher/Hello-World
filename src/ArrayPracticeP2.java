ea5b84feb7dbb65932b6c57e44178af71d6b09d2
adamiano <adamiano@student.mamkschools.org>
2018-09-20 09:50:40 -0400

    Project

---
+++ src/ArrayPracticeP2.java
@@ -0,0 +1,41 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+public class ArrayPracticeP2 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        userInput(console);
+    }
+
+    public static void userInput(Scanner console) {
+        System.out.print("How many days' temperatures? ");
+        int days = console.nextInt();
+        int[] temps = new int[days+1];
+        dayTemps(days, console, temps);
+    }
+
+    public static void dayTemps(int days, Scanner console, int[] temps) {
+        int highSum = 0;
+        int dayHigh;
+        for(int x=1; x<=days; x++) {
+            System.out.print("Day " + x + "'s high temp: ");
+            dayHigh = console.nextInt();
+            temps[x] = dayHigh;
+            highSum += dayHigh;
+        }
+        double averageTemp = (1.0 * highSum)/(1.0 *days);
+        System.out.println("Average temp = " + averageTemp);
+int daysAbove = 0;
+        for (int x=1; x<=days; x++) {
+            if (temps[x] > averageTemp) {
+                daysAbove++;
+            }
+        }
+        System.out.println(daysAbove + " days were above average.");
+        System.out.println("Tempuratures: ");
+
+        System.out.println(Arrays.toString(temps));
+        System.out.println("Two coldest days");
+
+    }
+}
---
+++ src/WeatherAnalysisP2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+//This program takes the use-input nd calculates the
+//average temperature for n-days. The program also identifies
+//how many days are above the average temperature.
+public class WeatherAnalysisP2 {
+    public static void main(String[] args) {
+        question();
+        Scanner console = new Scanner(System.in);
+    }
+
+    public static void question() {
+        System.out.println("How many days temperatures?");
+
+    }
+}
